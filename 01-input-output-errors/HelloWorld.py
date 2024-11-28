accb7eefdf3bb69df7901b9c20d38f700ffc4d1c
Katri Lotamois <katrilotamois@outlook.com>
2024-11-28 17:00:40 +0200

    Add function that prints Hello World

--- 01-input-output-errors/HelloWorld.py
+++ 01-input-output-errors/HelloWorld.py
@@ -1,2 +1,6 @@
+from HelloWorldTask import print_hello_world
+
+
+print_hello_world()
 print("Hello from Ultimate", "Ma mängin esimest korda", sep="\n")
 print("Git is good")
--- 01-input-output-errors/HelloWorldTask.py
+++ 01-input-output-errors/HelloWorldTask.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+# print("Hello World!")
+
+
+def print_hello_world():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    print_hello_world()
