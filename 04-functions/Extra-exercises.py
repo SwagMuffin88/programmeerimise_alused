e07bac0fd67af92016ac1980332b88ba492d1cc4
Katri Lotamõis <katrilotamois@outlook.com>
2025-12-02 12:55:52 +0200

    add banner exercise

---
+++ 04-functions/Extra-exercises.py
@@ -0,0 +1,24 @@
+"""Extra function exercises"""
+
+
+# Banner exercise
+def banner(slogan: str):
+    """Return uppercase version of entered string value."""
+    return str.upper(slogan)
+
+
+def show_banner():
+    """
+    Print slogan based on user input.
+    User is asked to input the slogan and nr of times it should be repeated. The method for returning
+    slogan as uppercase is then called and the returned result is printed to console the input nr of times.
+    """
+    num_of_repeats = input("How many times do you want to repeat your slogan? Enter: ")
+    slogan_input = input("Please enter your slogan: ") + ' '
+
+    print(banner(slogan_input) * int(num_of_repeats))
+
+if __name__ == '__main__':
+    show_banner()
+
+
