2e0b2f0e8b42919c920b648abc67941d5b712398
Katri Lotamõis <katrilotamois@outlook.com>
2025-12-16 10:00:44 +0200

    add ex. 5, 6

--- 05-Conditionals/cs_ut_exercises.py
+++ 05-Conditionals/cs_ut_exercises.py
@@ -1,3 +1,4 @@
+import math
 """Exercises from https://courses.cs.ut.ee/t/pythonkoolis/Main/SisendYl"""
 
 
@@ -119,7 +120,142 @@ def display_barks_n_times():
     print(("auh" + " ") * int(frequency))
 
 
+# Ex. 5
+def convert_temperatures():
+    """Ask user input for temp. units (start and dest.), temp. in start unit and return converted."""
+
+    while True:
+        end_unit = input("What units do you want to convert to? "
+                      "Enter 'c' for Celsius, 'f' for Fahrenheit or 'k' for Kelvin: ")
+        if end_unit != "c" and end_unit != "f" and end_unit != "k":
+            print("Please choose Celsius, Fahrenheit or Kelvin.")
+            continue
+        else:
+            break
+
+    while True:
+        start_unit = input("From what unit? "
+                      "Enter 'c' for Celsius, 'f' for Fahrenheit or 'k' for Kelvin: ")
+        if start_unit != "c" and start_unit != "f" and start_unit != "k":
+            print("Please choose Celsius, Fahrenheit or Kelvin.")
+            continue
+        else:
+            break
+
+    if start_unit == "c":
+        while True:
+            try:
+                celsius = float(input("Enter temperature in celsius: "))
+            except ValueError:
+                print("Please enter a numeral value.")
+                continue
+            else:
+                break
+
+        if end_unit == "f":
+            fahrenheit = (celsius * 1.8) + 32
+            print('%.2f Celsius is equivalent to: %.2f Fahrenheit'
+                  % (celsius, fahrenheit))
+
+        elif end_unit == "k":
+            kelvin = celsius + 273.15
+            print('%.2f Celsius is equivalent to: %.2f Kelvin'
+                  % (celsius, kelvin))
+
+    elif start_unit == "f":
+        while True:
+            try:
+                fahrenheit = float(input("Enter temperature in fahrenheit: "))
+            except ValueError:
+                print("Please enter a numeral value.")
+                continue
+            else:
+                break
+
+        if end_unit == "c":
+            celsius = (fahrenheit - 32) / 1.8
+            print('%.2f Fahrenheit is equivalent to: %.2f Celsius'
+              % (fahrenheit, celsius))
+
+        elif end_unit == "k":
+            kelvin = 273.5 + ((fahrenheit - 32.0) * (5.0 / 9.0))
+            print('%.2f Fahrenheit is equivalent to: %.2f Kelvin'
+                  % (fahrenheit, kelvin))
+
+
+    elif start_unit == "k":
+        while True:
+            try:
+                kelvin = float(input("Enter temperature in Kelvin: "))
+            except ValueError:
+                print("Please enter a numeral value.")
+                continue
+            else:
+                break
+
+        if end_unit == "c":
+            celsius = kelvin - 273.15
+            print('%.2f Kelvin is equivalent to: %.2f Celsius'
+              % (kelvin, celsius))
+
+        elif end_unit == "f":
+            fahrenheit = (9/5) * (kelvin - 273.15) + 32
+            print('%.2f Kelvin is equivalent to: %.2f Fahrenheit'
+                  % (kelvin, fahrenheit))
+
+
+
+# Ex. 6
+def calculate_discriminant(a: float, b: float, c: float) -> float:
+    return b ** 2 - 4 * a * c
+
+
+def is_valid_discriminant_result(discriminant: float):
+    if discriminant < 0:
+        return False
+    else:
+        return True
+
+
+def verify_user_input_for_quadratic() -> tuple[float, float, float]:
+    a = float(input("Sisesta ruutliige: "))
+    while a == 0:
+        print("Ruutliige ei tohi olla 0.")
+        a = float("Palun proovi uuesti: ")
+
+    b = float(input("Sisesta lineaarliige: "))
+    c = float(input("Sisesta vabaliige: "))
+
+    return a, b, c
+
+
+def solve_quadratic_formula(a, b, discriminant, use_addition):
+    if use_addition:
+        result = round(b * (-1) + math.sqrt(discriminant) / (2 * a), 2)
+    else:
+        result = round((b * (-1) - math.sqrt(discriminant) / (2 * a)), 2)
+
+    return result
+
+
+def verify_user_input_and_calculate_discriminant():
+    print("Arvutame ruutvõrrandit")
+    a, b, c = verify_user_input_for_quadratic()
+    discriminant = calculate_discriminant(a, b, c)
+
+    if is_valid_discriminant_result(discriminant):
+        print("Lahendid puuduvad")
+    elif discriminant == 0:
+        solution = solve_quadratic_formula(a, b, discriminant, True)
+        print(f"Lahendid on võrdsed: {solution}")
+    else:
+        solution_1 = solve_quadratic_formula(a, b, discriminant, True)
+        solution_2 = solve_quadratic_formula(a, b, discriminant, False)
+        print(f"Lahendeid on kaks: {solution_1} ja {solution_2}")
+
+
+
 if __name__ == '__main__':
     # calculate_rectangle_circumference_and_area()
     # greet_by_age()
-    # display_barks_n_times()
+    display_barks_n_times()
