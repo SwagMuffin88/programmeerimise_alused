01711d5e44a35789a176cebf2a32e74577e7a677
Katri <katrilotamois@outlook.com>
2026-03-24 11:17:59 +0200

    add class intro exercise

---
+++ 15-classes/Exercises.py
@@ -0,0 +1,9 @@
+"""Simple class."""
+
+
+class Student():
+    """Student class."""
+    def __init__(self, name = "Max", finished = False):
+        """Method to initialize a Student object."""
+        self.name = name
+        self.finished = finished
